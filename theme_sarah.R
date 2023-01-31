bb838dc85a658556b634aa47aa674f0b874f4bd1
Sarah McDonald <51337806+scmcdonald@users.noreply.github.com>
2023-01-30 17:46:29 -0800

    Update theme_sarah.R

--- theme_sarah.R
+++ theme_sarah.R
@@ -1,22 +1,13 @@
 theme_sarah <- function(){
-  # theme minimal makes background of plot white and does not distract from the figure
-  theme_minimal() %+replace% 
-  theme(
-    # set facet label size
-    strip.text = element_text(size = 16),
-    # set axis text sizes
-    axis.text=element_text(size=16),
-    # set axis text sizes and set to bold
-    axis.title=element_text(size=16,face="bold"),
-    axis.title.y = element_text(angle = 0, vjust = 0.6),
-    # increase space between facets
-    panel.spacing = unit(1, "lines"),
-    # set legend title  sizes and set to bold
-    legend.title = element_text(size=16,face="bold"),
-    # set legend text sizes
-    legend.text = element_text(size=16),
-    # set plot text sizes
-    plot.title = element_text(size = 20, face = "bold"),
-    # set plot title position to left corner
-    plot.title.position = "plot")
+  theme(legend.position = "none",
+        axis.text = element_text(size = 16), 
+        axis.title = element_text(size = 16, face = "bold"), 
+        plot.title = element_text(size = 20, face = "bold"), 
+        plot.subtitle = element_text(size = 16), 
+        axis.ticks=element_blank(),
+        # set plot title position to left corner
+        plot.title.position = "plot", 
+        panel.background = element_rect(fill = "white"), 
+        panel.grid.major = element_line(color="grey90"), 
+        panel.grid.minor = element_line(color="grey90")) 
 }
